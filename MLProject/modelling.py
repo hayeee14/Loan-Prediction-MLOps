71fd50bb2f3c759fc8b3f7de7561b176aed6cb06
Rayhan <rayhanm08m@gmail.com>
2025-12-20 02:35:06 +0700

    Fix: run python directly to avoid mlflow database lag

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,14 +3,14 @@ import mlflow.sklearn
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-def train():
+if __name__ == "__main__":
     with mlflow.start_run():
+        
         df = pd.DataFrame({'Income': [5000, 2000], 'Status': [1, 0]})
         X, y = df[['Income']], df['Status']
         model = RandomForestClassifier().fit(X, y)
+        
+        # Log parameter dan metric
         mlflow.log_metric("accuracy", 1.0)
-        mlflow.sklearn.log_model(model, "loan_model")
-        print("Retraining Selesai!")
-
-if __name__ == "__main__":
-    train()
+        mlflow.sklearn.log_model(model, "model")
+        print("Retraining Berhasil!")
